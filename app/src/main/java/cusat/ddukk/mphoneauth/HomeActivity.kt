1fbf29a2fffceea8c73f3f24fbd44fa71bfeb118
anwar <anwarnsyd@gmail.com>
2024-06-26 11:44:06 +0530

    updated

--- app/src/main/java/cusat/ddukk/mphoneauth/HomeActivity.kt
+++ app/src/main/java/cusat/ddukk/mphoneauth/HomeActivity.kt
@@ -2,11 +2,14 @@ package cusat.ddukk.mphoneauth
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,9 @@ class HomeActivity : AppCompatActivity() {
         val btnStartWorkout = findViewById<Button>(R.id.btnStartWorkout)
         val btnViewProgress = findViewById<Button>(R.id.btnViewProgress)
         val btnSetGoals = findViewById<Button>(R.id.btnSetGoals)
+
+//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+//        setSupportActionBar(toolbar)
 
         btnStartWorkout.setOnClickListener {
 //            val intent = Intent(this, StartWorkoutActivity::class.java)
@@ -38,7 +44,26 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this, GoalsActivity::class.java)
             startActivity(intent)
         }
-
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.Logoutid -> {
+                Toast.makeText(applicationContext, "Logout Successfully ", Toast.LENGTH_SHORT).show()
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
